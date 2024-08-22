1164fc7105df33cace1b35f98f82e94c4543c3d2
isadora.ribeiro@automind.com.br <isadora.ribeiro@automind.com.br>
2024-08-22 13:59:52 -0300

    atualização

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,19 @@
+Cypress.Commands.add('login', (
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password'),
+) => {
+    const login = () => {
+        cy.visit('/users/sign_in')
+
+        cy.get("[data-qa-selector='login_field']").type(user)
+        cy.get("[data-qa-selector='password_field']").type(password, {log:false})
+        cy.get("[data-qa-selector='sign_in_button']").click()
+        }
+
+        login()
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+})
