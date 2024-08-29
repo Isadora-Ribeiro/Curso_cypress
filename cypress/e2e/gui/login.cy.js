634402de916c4d9d010161d89e96965e05115586
isadora.ribeiro@automind.com.br <isadora.ribeiro@automind.com.br>
2024-08-29 07:37:14 -0300

    arquivos

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,7 +1,13 @@
 describe('Login', () => {
   it('successfully', () => {
-    cy.login()
 
-    cy.get('.qa-user-avatar').should('be.visible')
-  })
+
+  const user = Cypress.env('user_name')
+  const password = Cypress.env('user_password')
+  const options = { cacheSession: false }
+
+  cy.login(user, password, options )
+
+  cy.get('.qa-user-avatar').should('be.visible')
+ }) 
 })
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
+    { cacheSession = true } = {}, 
 ) => {
     const login = () => {
         cy.visit('/users/sign_in')
@@ -8,12 +9,44 @@ Cypress.Commands.add('login', (
         cy.get("[data-qa-selector='login_field']").type(user)
         cy.get("[data-qa-selector='password_field']").type(password, {log:false})
         cy.get("[data-qa-selector='sign_in_button']").click()
-        }
+     }
 
+     const validate = () => {
+        cy.visit('/')
+        cy.location('pathname', { timeout: 1000 })
+          .should('not.eq', '/users/sign_in')
+     }
+
+     const options = {
+        cacheAcrossSpecs: true,
+        validate,
+     }
+
+     if(cacheSession) {
+        cy.session(user, login, options)
+     } else {
         login()
+     }
 })
 
 Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+}) 
+
+Cypress.Commands.add('gui_createIssue', issue => {
+    cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+    cy.get('.qa-issuable-from-title').type(issue.title)
+    cy.get('.qa-issuable-from-description').type(issue.description)
+    cy.contains('Submite issue').click()
 })
